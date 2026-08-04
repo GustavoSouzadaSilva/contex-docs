e26d5fc4e242aba6f0ec959b1fcf8880c74332d3
GustavoSouzadaSilva <gustavo@contexdigital.com>
2026-08-04 15:01:27 -0300

    Adiciona js/app.js

---
+++ js/app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const botoesConfissao = [
+    document.getElementById("btnConfissao"),
+    document.getElementById("btnNovaConfissao")
+].filter(Boolean);
+
+const botoesContratos = [
+    document.getElementById("btnContratos"),
+    document.getElementById("menuContratos")
+].filter(Boolean);
+
+botoesConfissao.forEach((botao) => {
+    botao.addEventListener("click", () => {
+        window.location.href = "pages/confissao.html";
+    });
+});
+
+botoesContratos.forEach((botao) => {
+    botao.addEventListener("click", () => {
+        window.alert("Módulo em desenvolvimento.");
+    });
+});
+
